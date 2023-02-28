f5d1f68dffe41507695c0ea240a2e11e096632c1
kklevi96 <kklevi@gmail.com>
2023-02-28 21:00:18 +0100

    delete all

--- Pasteimg.Server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Pasteimg.Server.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
