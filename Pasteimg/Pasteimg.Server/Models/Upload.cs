5231070a579074a013a009069e92449772f7cd3f
kklevi96 <kklevi@gmail.com>
2023-02-28 21:07:45 +0100

    Upload model base recreated

---
+++ Pasteimg/Pasteimg.Server/Models/Upload.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pasteimg.Server.Models
+{
+    public class Upload
+    {
+        public Upload()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [NotMapped]
+        public virtual List<Image> Images { get; set; }
+    }
+}
