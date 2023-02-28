a2691ac6e494943e80fb75ddd40db6b50a4bd818
kklevi96 <kklevi@gmail.com>
2023-02-28 21:07:37 +0100

    Image model base recreated

---
+++ Pasteimg/Pasteimg.Server/Models/Image.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pasteimg.Server.Models
+{
+    public class Image
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        public bool NSFW { get; set; }
+
+        [MaxLength(120)]
+        public string Description { get; set; }
+
+        [NotMapped]
+        public virtual Upload Upload { get; set; }
+
+        [ForeignKey(nameof(Upload))]
+        public string UploadID { get; set; }
+    }
+}
