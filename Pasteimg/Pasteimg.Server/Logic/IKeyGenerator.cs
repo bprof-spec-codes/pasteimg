257dc8cde174778a0c332d857ab467afb5c1e3ba
beresB <57649310+beresB@users.noreply.github.com>
2023-03-01 09:12:22 +0100

    Kulcsot generál guiddal, biztonság kedvéért megnézi azért előtte, hogy szerepel-e már a kollekcióban, mivel úgy sejtem, hogy csak a program futásáig garantálja az egyediséget, így a perzisztenciánál potenciális probléma lehet, mégha gyakorlatilag 0 is az esélye.

---
+++ Pasteimg/Pasteimg.Server/Logic/IKeyGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace Pasteimg.Server.Logic
+{
+    public interface IKeyGenerator
+    {
+        string GenerateKey(IEnumerable<string> collection);
+    }
+    public class KeyGenerator : IKeyGenerator
+    {
+        public string GenerateKey(IEnumerable<string> collection)
+        {
+            string key;
+
+            do
+            {
+                key = Guid.NewGuid().ToString().Replace("-","");
+
+            }
+            while (collection.Contains(key));
+            return key;
+
+        }
+    }
+}
