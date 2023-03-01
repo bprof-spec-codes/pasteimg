db8bad87ddd20edcd935bbeaa9120b905afb465b
beresB <57649310+beresB@users.noreply.github.com>
2023-03-01 09:15:45 +0100

    Appdb-hez hozzá lett adva a model.

--- Pasteimg/Pasteimg.Server/Data/ApplicationDbContext.cs
+++ Pasteimg/Pasteimg.Server/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Pasteimg.Server.Models;
 
 namespace Pasteimg.Server.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public virtual DbSet<BinaryImage> Images { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
