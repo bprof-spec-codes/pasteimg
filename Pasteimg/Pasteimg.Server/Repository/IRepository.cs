e5329291daa770969ae2fa2dcf97f613f90a6e1f
beresB <57649310+beresB@users.noreply.github.com>
2023-03-01 09:10:02 +0100

    Egyszerű dbcontextes repo, semmi extra.

---
+++ Pasteimg/Pasteimg.Server/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Pasteimg.Server.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity item);
+        TEntity? Delete(params object[] id);
+        TEntity? Read(params object[] id);
+        IQueryable<TEntity> ReadAll();
+        void Update(Action<TEntity> updateAction, params object[] id);
+    }
+}
---
+++ Pasteimg/Pasteimg.Server/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pasteimg.Server.Data;
+
+namespace Pasteimg.Server.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private DbContext context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public void Create(TEntity item)
+        {
+            context.Set<TEntity>().Add(item);
+            context.SaveChanges();
+        }
+
+        public TEntity? Delete(params object[] id)
+        {
+            TEntity? entity = Read(id);
+            if (entity != null)
+            {
+                context.Set<TEntity>().Remove(entity);
+                context.SaveChanges();
+            }
+            return entity;
+        }
+
+        public void Update(Action<TEntity> updateAction, params object[] id)
+        {
+            TEntity? entity = Read(id);
+            if (entity != null)
+            {
+                updateAction(entity);
+                context.SaveChanges();
+            }
+        }
+        public TEntity? Read(params object[] id)
+        {
+            return context.Set<TEntity>().Find(id);
+        }
+
+        public IQueryable<TEntity> ReadAll()
+        {
+            return context.Set<TEntity>();
+        }
+
+
+    }
+}
