3b52f07978719c4510aef0f70228a32edb5d88e5
beresB <57649310+beresB@users.noreply.github.com>
2023-03-01 09:09:41 +0100

    Képeket és thumbnailt binárisan tárolja. Gondolkoztam alternatívaként, hogy csak url tárol és az lenne az elsődleges kulcs is, de akkor az adatbázis mellett a fájlrendszerrel is szarakodni kell, hogy szinkronban maradjon. Szóval így talán egyszerűbb.

---
+++ Pasteimg/Pasteimg.Server/Models/BinaryImage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pasteimg.Server.Models
+{
+    [Index(nameof(GroupKey), IsUnique = false)]
+    [Index(nameof(Timestamp), IsUnique = false)]
+    public class BinaryImage
+    {
+        [Key]
+        [Required(AllowEmptyStrings = false)]
+        public string Key { get; set; }
+        [Required]
+        public byte[] Content { get; set; }
+        public byte[]? Thumbnail { get; set; }
+        public bool IsNsfw { get; set; }
+        public string? Password { get; set; }
+        [StringLength(120)]
+        public string? Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public DateTime LastAccess { get; set; }
+        public string? GroupKey { get; set; }
+    }
+}
