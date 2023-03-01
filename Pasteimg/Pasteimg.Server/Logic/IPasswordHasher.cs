4eaa2c24d84475242601a3cdc99baec0fd0adf38
beresB <57649310+beresB@users.noreply.github.com>
2023-03-01 09:14:28 +0100

    Összegányolt sha256 hashfüggvény determinisztikus randommal sózva, talán a célnak megfelel.

---
+++ Pasteimg/Pasteimg.Server/Logic/IPasswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace Pasteimg.Server.Logic
+{
+    public interface IPasswordHasher
+    {
+        string? CreateHash(string? password);
+    }
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string? CreateHash(string? password)
+        {
+            if (!string.IsNullOrEmpty(password))
+            {
+                using (SHA256 hash = SHA256.Create())
+                {
+
+                    byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                    Random random = new Random(password.Sum(c => ~c));
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+
+                        bytes[i] = (byte)(~random.Next(bytes.Length));
+                    }
+
+                    return BitConverter.ToString(bytes).Replace("-","").ToLower();
+                }
+            }
+            else return null;
+        }
+         
+        }
+    }
