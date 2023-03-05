8fcc33776174e46697156894566ec14c1c5254ac
beresB <57649310+beresB@users.noreply.github.com>
2023-03-05 07:54:45 +0100

    DebugLogic added

---
+++ Pasteimg/Pasteimg.Server/Logic/DebugLogic.cs
@@ -0,0 +1,27 @@
+﻿using Pasteimg.Server.Models;
+using Pasteimg.Server.Repository;
+
+namespace Pasteimg.Server.Logic
+{
+    public class DebugLogic : PasteImgLogic
+    {
+        public DebugLogic(IPasswordHasher passwordHasher,
+                          IImageProcessor processor,
+                          IImageFileStorage fileStorage) : base(new DebugRepository<Image>(), new DebugRepository<Upload>(), passwordHasher, processor, fileStorage)
+        {
+            imageResults = new Dictionary<string, CompressionResult>();
+            thumbnailResults = new Dictionary<string, ProcessorResult>();
+        }
+        Dictionary<string, CompressionResult> imageResults;
+        Dictionary<string, ProcessorResult> thumbnailResults;
+        
+        public override void UploadImages(UploadModel upload)
+        {
+            base.UploadImages(upload, (img, thumb,key) =>
+            {
+                imageResults.Add(key,img);
+                thumbnailResults.Add(key,thumb);
+            });
+        }
+    }
+}
