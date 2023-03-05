75d1d1fc1e249a72a917fe3395181c299cbe8f4d
beresB <57649310+beresB@users.noreply.github.com>
2023-03-05 07:59:26 +0100

    IFormFileExtension added

---
+++ Pasteimg/Pasteimg.Server/Models/IFormFileExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Reflection;
+
+namespace Pasteimg.Server.Models
+{
+    public static class IFormFileExtension
+    {
+        public static IFormFileCollection? GetFiles(this IValueProvider? provider)
+        {
+            if (provider is FormValueProvider formValueProvider)
+            {
+                return (formValueProvider.GetType()
+                        .GetField("_values", BindingFlags.NonPublic | BindingFlags.Instance)?
+                        .GetValue(formValueProvider) as FormCollection)?.Files;
+            }
+            else if (provider is CompositeValueProvider compositeProvider)
+            {
+                return GetFiles(compositeProvider.FirstOrDefault(vp => vp is FormValueProvider));
+            }
+            else return null;
+        }
+
+        public static byte[] ToArray(this IFormFile file)
+        {
+            using (Stream stream = file.OpenReadStream())
+            {
+                using (MemoryStream destination = new MemoryStream())
+                {
+                    stream.CopyTo(destination);
+                    return destination.ToArray();
+                }
+            }
+        }
+    }
+
+}
