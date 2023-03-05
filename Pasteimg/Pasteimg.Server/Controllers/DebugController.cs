cb020f32909911d93e2461442b9b5d6802984013
beresB <57649310+beresB@users.noreply.github.com>
2023-03-05 07:55:02 +0100

    DebugController added

---
+++ Pasteimg/Pasteimg.Server/Controllers/DebugController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pasteimg.Server.Logic;
+using Pasteimg.Server.Models;
+
+namespace Pasteimg.Server.Controllers
+{
+    public class DebugController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly DebugLogic _logic;
+        public DebugController(ILogger<HomeController> logger, DebugLogic logic)
+        {
+            _logger = logger;
+            _logic = logic;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetBlankItem()
+        {
+            return PartialView("_BlankItem", new UploadModel());
+        }
+
+        public IActionResult Upload()
+        {
+            return View("UploadModel",new UploadModel());
+        }
+
+        [HttpPost]
+        public IActionResult SubmitUpload(UploadModel upload)
+        {
+            return RedirectToAction(nameof(Upload));
+        }
+        public IActionResult UploadList()
+        {
+            throw new NotImplementedException();
+        }
+        public IActionResult Images(string uploadKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
