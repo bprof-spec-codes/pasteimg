74f4b347c081d23c71cf8fa102d9bcf7dd827a5b
beresB <57649310+beresB@users.noreply.github.com>
2023-03-11 17:03:11 +0100

    Entity configurations added.

---
+++ Pasteimg/Pasteimg.Server/Data/Configurations/ImageConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pasteimg.Server.Models;
+
+namespace Pasteimg.Server.Data.Configurations
+{
+    public class ImageConfiguration : IEntityTypeConfiguration<Image>
+    {
+        public void Configure(EntityTypeBuilder<Image> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.HasOne(i => i.OptimizationResult)
+                   .WithOne(o => o.Image)
+                   .HasForeignKey(typeof(OptimizationResult), nameof(OptimizationResult.Id))
+                   .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
---
+++ Pasteimg/Pasteimg.Server/Data/Configurations/UploadConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pasteimg.Server.Models;
+
+namespace Pasteimg.Server.Data.Configurations
+{
+    public class UploadConfiguration : IEntityTypeConfiguration<Upload>
+    {
+        public void Configure(EntityTypeBuilder<Upload> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.HasMany(u => u.Images)
+                   .WithOne(i => i.Upload)
+                   .HasForeignKey(i => i.UploadID)
+                   .OnDelete(DeleteBehavior.Cascade);
+            builder.HasIndex(u => u.TimeStamp);
+        }
+    }
+}
