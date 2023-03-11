23f53af4737df3d3751fd860cece82ab58cb7c35
beresB <57649310+beresB@users.noreply.github.com>
2023-03-11 17:03:41 +0100

    ApplicationDbContext changed.

--- Pasteimg/Pasteimg.Server/Data/ApplicationDbContext.cs
+++ Pasteimg/Pasteimg.Server/Data/ApplicationDbContext.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Pasteimg.Server.Models;
+using System.Runtime.CompilerServices;
 
 namespace Pasteimg.Server.Data
 {
+
     public class ApplicationDbContext : IdentityDbContext
     {
-        public virtual DbSet<Upload> Uploads { get; set; }
-        public virtual DbSet<Image> Images { get; set; }
-
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetAssembly(typeof(ApplicationDbContext)));
+        }
+        public virtual DbSet<Image> Images { get; set; }
+        public virtual DbSet<Upload> Uploads { get; set; }
+        public virtual DbSet<OptimizationResult> OptimizationResults { get; set; }
     }
 }
