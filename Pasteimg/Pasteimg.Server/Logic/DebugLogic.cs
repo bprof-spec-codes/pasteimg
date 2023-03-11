66b7e1fb835a4127343a2d329a0574cb78f0adc6
beresB <57649310+beresB@users.noreply.github.com>
2023-03-11 17:04:04 +0100

    DebugLogic changed.

--- Pasteimg/Pasteimg.Server/Logic/DebugLogic.cs
+++ Pasteimg/Pasteimg.Server/Logic/DebugLogic.cs
@@ -1,27 +1,47 @@
 ﻿using Pasteimg.Server.Models;
 using Pasteimg.Server.Repository;
+using Pasteimg.Server.Repository.IFileStorage;
 
 namespace Pasteimg.Server.Logic
 {
-    public class DebugLogic : PasteImgLogic
+    public class DebugLogic:PasteImgLogic
     {
-        public DebugLogic(IPasswordHasher passwordHasher,
-                          IImageProcessor processor,
-                          IImageFileStorage fileStorage) : base(new DebugRepository<Image>(), new DebugRepository<Upload>(), passwordHasher, processor, fileStorage)
+
+        public DebugLogic(IRepository<Image> imageRepository,
+                             IRepository<Upload> uploadRepository,
+                             IRepository<OptimizationResult> optimizationRepository,
+                             IImageFileStorageFactory fileStorageFactory) :
+            base(imageRepository, uploadRepository, optimizationRepository,
+                fileStorageFactory.CreateImagesStorage(Path.Combine("deb","images"), 1500, 1500),
+                fileStorageFactory.CreateThumbnailsStorage(Path.Combine("deb","images_thumbnails"), 300, 300))
         {
-            imageResults = new Dictionary<string, CompressionResult>();
-            thumbnailResults = new Dictionary<string, ProcessorResult>();
-        }
-        Dictionary<string, CompressionResult> imageResults;
-        Dictionary<string, ProcessorResult> thumbnailResults;
-        
-        public override void UploadImages(UploadModel upload)
-        {
-            base.UploadImages(upload, (img, thumb,key) =>
+            if (imageRepository.ReadAll().Count() == 0)
             {
-                imageResults.Add(key,img);
-                thumbnailResults.Add(key,thumb);
-            });
+                images.ClearRoot();
+                thumbnails.ClearRoot();
+                AddSampleImagesAsync().Wait();
+            }
         }
+
+        private async Task AddSampleImagesAsync()
+        {
+            Upload upload = new Upload();
+            var files = Directory.GetFiles(Path.Combine("wwwRoot", "deb", "optimizer_input"));
+            upload.Password = "debug";
+            Random rnd = new Random();
+            foreach (var item in files)
+            {
+                MemoryStream stream = new MemoryStream(File.ReadAllBytes(item));
+                string fileName = Path.GetFileName(item);
+                upload.Images.Add(new Image()
+                {
+                    Description = fileName,
+                    Content = new FormFile(stream, 0, stream.Length, fileName, fileName),
+                    NSFW = rnd.Next(0, 2) == 1
+                });
+            }
+            await UploadAndStoreResultsAsync(upload);
+        }
+
     }
 }
