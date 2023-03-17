89b6f2610b09b229c2680ae4409b598c01b31fa5
beresB <57649310+beresB@users.noreply.github.com>
2023-03-17 18:51:38 +0100

    Upload moved.

--- Pasteimg/Pasteimg.Server/Models/Entity/Upload.cs
+++ Pasteimg/Pasteimg.Server/Models/Entity/Upload.cs
@@ -3,28 +3,28 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Pasteimg.Server.Models
+namespace Pasteimg.Server.Models.Entity
 {
     [ModelBinder(typeof(UploadModelBinder))]
-    public class Upload :IEntity
+    public class Upload : IEntity
     {
-        [ValidateNever, StringLength(32, MinimumLength = 32)]
-        public string Id { get; set; }
-        public string? Password { get; set; }
-        public DateTime TimeStamp { get; set; }
-
         public Upload()
         {
             Images = new List<Image>();
         }
 
+        [ValidateNever, StringLength(32, MinimumLength = 32)]
+        public string Id { get; set; }
+
         [NotMapped]
         public virtual IList<Image> Images { get; set; }
+
+        public string? Password { get; set; }
+        public DateTime TimeStamp { get; set; }
 
         public object[] GetKey()
         {
             return new object[] { Id };
         }
     }
-  
 }
