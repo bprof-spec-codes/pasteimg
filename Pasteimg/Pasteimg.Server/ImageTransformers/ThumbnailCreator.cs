51ad829ed734751443a3f3866ba3e40d192468c8
beresB <57649310+beresB@users.noreply.github.com>
2023-03-18 00:27:30 +0100

    ThumbnailCreator added.

---
+++ Pasteimg/Pasteimg.Server/ImageTransformers/ThumbnailCreator.cs
@@ -0,0 +1,34 @@
+﻿using ImageMagick;
+
+namespace Pasteimg.Server.Transformers
+{
+    public class ThumbnailCreator : WebImageTransformer
+    {
+        public ThumbnailCreator(int maxWidth, int maxHeight, int quality) : base(maxWidth, maxHeight, quality)
+        { }
+
+        public override string Transform(string path)
+        {
+            using MagickImageCollection frames = new MagickImageCollection(path);
+            TransformMethod(frames);
+            frames.Write(path);
+            return path;
+        }
+
+        public override byte[] Transform(byte[] content)
+        {
+            using MagickImageCollection frames = new MagickImageCollection(content);
+            TransformMethod(frames);
+            return frames.ToByteArray();
+        }
+
+        private void TransformMethod(MagickImageCollection frames)
+        {
+            Parallel.ForEach(frames, frame =>
+            {
+                frame.Quality = Quality;
+                frame.Thumbnail(MaxWidth, MaxHeight);
+            });
+        }
+    }
+}
