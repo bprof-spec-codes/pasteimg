b385e64e341280195a99dbe7d10e1c99fc21fcac
beresB <57649310+beresB@users.noreply.github.com>
2023-03-18 00:28:10 +0100

    BackgroundTransformationRequest added.

---
+++ Pasteimg/Pasteimg.Server/ImageTransformers/BackgroundTransfromationRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Pasteimg.Server.Transformers
+{
+    public class BackgroundTransformationRequest
+    {
+        public BackgroundTransformationRequest(string path, IImageTransformer transformer)
+        {
+            Path = path;
+            Transformer = transformer;
+        }
+
+        public string Path { get; }
+        public IImageTransformer Transformer { get; }
+
+        public string Transform()
+        {
+            return Transformer.Transform(Path);
+        }
+    }
+}
