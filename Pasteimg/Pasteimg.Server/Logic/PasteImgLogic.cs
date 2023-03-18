fd9ab09b41219f30ec70ae61560ecfd3c1306b49
beresB <57649310+beresB@users.noreply.github.com>
2023-03-18 01:11:24 +0100

    PasteImgLogic changed.

--- Pasteimg/Pasteimg.Server/Logic/PasteImgLogic.cs
+++ Pasteimg/Pasteimg.Server/Logic/PasteImgLogic.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Pasteimg.Server.Configurations;
 using Pasteimg.Server.Models;
+using Pasteimg.Server.Models.Entity;
+using Pasteimg.Server.Models.Error;
 using Pasteimg.Server.Repository;
-using Pasteimg.Server.Repository.IFileStorage;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+using Pasteimg.Server.Repository.FileStorages;
+using Pasteimg.Server.Transformers;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -11,237 +12,60 @@ namespace Pasteimg.Server.Logic
 {
     public interface IPasteImgLogic
     {
-        long? MaxFileSize { get; set; }
-        int? MaxImagePerUpload { get; set; }
+        PasteImgConfiguration Configuration { get; }
+
+        string? CreateHash(string? password);
 
         void DeleteImage(string id);
-        void DeleteOptimizationResult(string id);
+
         void DeleteUpload(string id);
-        string? GetImagePath(Image image);
-        string? GetImagePath(string id);
-        string? GetThumbnailPath(Image image);
-        string? GetThumbnailPath(string id);
-        IEnumerable<Image> ReadAllImage();
-        IEnumerable<OptimizationResult> ReadAllOptimizationResult();
-        IEnumerable<Upload> ReadAllUpload();
-        Image? ReadImage(string id);
-        OptimizationResult? ReadOptimizationResult(string id);
-        Upload? ReadUpload(string id);
-        void Upload(Upload upload);
-        void UploadAndStoreResults(Upload upload);
-        Task UploadAndStoreResultsAsync(Upload upload);
+
+        IEnumerable<Image> GetAllImage();
+
+        IEnumerable<Upload> GetAllUpload();
+
+        Image GetImage(string id);
+
+        Image GetImageWithSourceFile(string id);
+
+        Image GetImageWithThumbnailFile(string id);
+
+        Upload GetUpload(string id);
+
+        Upload GetUploadWithSourceFiles(string id);
+
+        Upload GetUploadWithThumbnailFiles(string id);
+
+        ValidationConfiguration GetValidationConfiguration();
+
+        void PostUpload(Upload upload);
     }
+
     public class PasteImgLogic : IPasteImgLogic
     {
-        protected const long MegaByte = 1_000_000;
-        protected readonly IRepository<Image> imageRepository;
+        protected readonly IBackgroundTransformer backgroundTransformer;
+        protected readonly IImageFileRepository imageRepository;
+        protected readonly WebImageTransformer sourceOptimizer;
+        protected readonly WebImageTransformer thumbnailCreator;
         protected readonly IRepository<Upload> uploadRepository;
-        protected readonly IRepository<OptimizationResult> optimizationRepository;
-        protected readonly IImageFileStorage images;
-        protected readonly IImageFileStorage thumbnails;
 
-        public PasteImgLogic(IRepository<Image> imageRepository,
-                             IRepository<Upload> uploadRepository,
-                             IRepository<OptimizationResult> optimizationRepository,
-                             IImageFileStorageFactory fileStorageFactory) :
-            this(imageRepository, uploadRepository, optimizationRepository,
-                fileStorageFactory.CreateImagesStorage("images",1500, 1500), 
-                fileStorageFactory.CreateThumbnailsStorage("images_thumbnails",300, 300))
-        {
-        }
+        public PasteImgLogic(IRepository<Upload> uploadRepository,
+                             IImageFileRepositoryFactory fileStorageFactory,
+                             IWebImageTransformerFactory transformerFactory,
+                             IBackgroundTransformer backgroundTransformer)
 
-        protected PasteImgLogic(IRepository<Image> imageRepository,
-                            IRepository<Upload> uploadRepository,
-                            IRepository<OptimizationResult> optimizationRepository,
-                            IImageFileStorage images,
-                            IImageFileStorage thumbnails)
         {
-            this.imageRepository = imageRepository;
+            Configuration = ReadConfiguration();
             this.uploadRepository = uploadRepository;
-            this.optimizationRepository = optimizationRepository;
-            this.images = images;
-            this.thumbnails = thumbnails;
+            this.backgroundTransformer = backgroundTransformer;
+            imageRepository = fileStorageFactory.CreateImageFileRepository(Configuration.Storage);
+            thumbnailCreator = transformerFactory.CreateThumbnailCreator(Configuration.Transformation);
+            sourceOptimizer = transformerFactory.CreateSourceOptimizer(Configuration.Transformation);
         }
 
-        public long? MaxFileSize { get; set; } = 10 * MegaByte;
-        public int? MaxImagePerUpload { get; set; } = 50;
+        public PasteImgConfiguration Configuration { get; }
 
-
-        public virtual void DeleteOptimizationResult(string id)
-        {
-            optimizationRepository.Delete(id);
-        }
-        public virtual void DeleteImage(string id)
-        {
-
-            if (imageRepository.Delete(id) is Image image)
-            {
-                images.DeleteFile(image.Id);
-                thumbnails.DeleteFile(image.Id);
-            };
-        }
-
-        public virtual void DeleteUpload(string id)
-        {
-            if (uploadRepository.Delete(id) is Upload upload)
-            {
-                foreach (var item in upload.Images)
-                {
-                    DeleteImage(item.Id);
-                }
-            };
-        }
-
-        public string? GetImagePath(Image image)
-        {
-            return GetImagePath(image.Id);
-        }
-
-        public string? GetImagePath(string id)
-        {
-            return images.FindWebFile(id);
-        }
-
-
-        public string? GetThumbnailPath(Image image)
-        {
-            return GetThumbnailPath(image.Id);
-        }
-
-        public string? GetThumbnailPath(string id)
-        {
-            return thumbnails.FindWebFile(id);
-        }
-
-        public IEnumerable<Image> ReadAllImage()
-        {
-            return imageRepository.ReadAll();
-        }
-
-        public IEnumerable<Upload> ReadAllUpload()
-        {
-            return uploadRepository.ReadAll();
-        }
-        public IEnumerable<OptimizationResult> ReadAllOptimizationResult()
-        {
-            return optimizationRepository.ReadAll();
-        }
-        public Image? ReadImage(string id)
-        {
-            return imageRepository.Read(id);
-        }
-
-        public Upload? ReadUpload(string id)
-        {
-            return uploadRepository.Read(id);
-        }
-        public OptimizationResult? ReadOptimizationResult(string id)
-        {
-            return optimizationRepository.Read(id);
-        }
-      
-        public void Upload(Upload upload)
-        {
-            ValidateUpload(upload);
-            SetUpload(upload);
-            SetImages(upload, out FileArgument[] files);
-            var thumbResults = files.Select(f => thumbnails.OptimizeImage(f.Content, f.Id)).ToArray();
-            thumbnails.SaveFiles(thumbResults);
-            var results = files.Select(f => images.OptimizeImage(f.Content, f.Id)).ToArray();
-            images.SaveFiles(results);
-            uploadRepository.Create(upload);
-
-            CleanContents(upload.Images, thumbResults, results);
-        }
-
-        public async Task UploadAsync(Upload upload)
-        {
-            ValidateUpload(upload);
-            SetUpload(upload);
-            SetImages(upload, out FileArgument[] files);
-            var thumbResults = await thumbnails.OptimizeImagesAsync(files);
-            thumbnails.SaveFiles(thumbResults);
-            images.SaveFiles(files);
-            var results = await images.OptimizeImagesAsync(files);
-            images.SaveFiles(results);
-            uploadRepository.Create(upload);
-
-            CleanContents(upload.Images, thumbResults, results);
-        }
-        public void UploadAndStoreResults(Upload upload)
-        {
-            ValidateUpload(upload);
-            SetUpload(upload);
-            SetImages(upload, out FileArgument[] files);
-            var thumbResults = files.Select(f => thumbnails.OptimizeImage(f.Content, f.Id)).ToArray();
-            thumbnails.SaveFiles(thumbResults);
-            var results = files.Select(f => images.OptimizeImage(f.Content, f.Id)).ToArray();
-            images.SaveFiles(results);
-            uploadRepository.Create(upload);
-
-            foreach (var item in results)
-            {
-                optimizationRepository.Create(item);
-            }
-            CleanContents(upload.Images, thumbResults, results);
-        }
-        public async Task UploadAndStoreResultsAsync(Upload upload)
-        {
-            ValidateUpload(upload);
-            SetUpload(upload);
-            SetImages(upload, out FileArgument[] files);
-            var thumbResults = await thumbnails.OptimizeImagesAsync(files);
-            thumbnails.SaveFiles(thumbResults);
-            images.SaveFiles(files);
-            var results=await images.OptimizeImagesAsync(files);
-            images.SaveFiles(results);
-            uploadRepository.Create(upload);
-
-            foreach (var item in results)
-            {
-                optimizationRepository.Create(item);
-            }
-
-            CleanContents(upload.Images, thumbResults, results);
-        }
-        private void CleanContents(IList<Image> images,OptimizationResult[] thumbnailResults, OptimizationResult[] imagesResults)
-        {
-            for (int i = 0; i < images.Count; i++)
-            {
-                images[i].Content = null;
-                thumbnailResults[i].Content = null;
-                imagesResults[i].Content = null;
-            }
-        }
-
-  
-     
-        protected virtual void ValidateUpload(Upload upload)
-        {
-        }
-
-        private void SetUpload(Upload upload)
-        {
-            upload.Id = GenerateId(uploadRepository.ReadAll().Select(i => i.Id));
-            upload.Password = CreateHash(upload.Password);
-            upload.TimeStamp = DateTime.Now;
-        }
-        private void SetImages(Upload upload, out FileArgument[] files)
-        {
-            int count = upload.Images.Count;
-            files = new FileArgument[count];
-            string[] ids = GenerateMultipleId(ReadAllImage().Select(i => i.Id), count);
-            for (int i = 0; i < count; i++)
-            {
-                Image image = upload.Images[i];
-                image.Id = ids[i];
-                image.UploadID = upload.Id;
-                files[i] = new FileArgument(image.Content.ToArray(),
-                                            ids[i],
-                                            Path.GetFileNameWithoutExtension(image.Content.FileName));
-            }
-        }
-        private string? CreateHash(string? password)
+        public string? CreateHash(string? password)
         {
             if (!string.IsNullOrEmpty(password))
             {
@@ -269,38 +93,198 @@ namespace Pasteimg.Server.Logic
             else return null;
         }
 
-        private string GenerateId(IEnumerable<string> collection)
+        public virtual void DeleteImage(string id)
+        {
+            Image? image = imageRepository.Delete(id);
+            if (image is null)
+            {
+                throw new NotFoundException(typeof(Image), id);
+            }
+        }
+
+        public virtual void DeleteUpload(string id)
+        {
+            if (uploadRepository.Delete(id) is Upload upload)
+            {
+                foreach (var item in upload.Images)
+                {
+                    imageRepository.Delete(item.Id);
+                }
+            }
+            else
+            {
+                throw new NotFoundException(typeof(Upload), id);
+            }
+        }
+
+        public IEnumerable<Image> GetAllImage()
+        {
+            return imageRepository.ReadAll();
+        }
+
+        public IEnumerable<Upload> GetAllUpload()
+        {
+            return uploadRepository.ReadAll();
+        }
+
+        public Image GetImage(string id)
+        {
+            Image? image = imageRepository.Read(id);
+            if (image is null)
+            {
+                throw new NotFoundException(typeof(Image), id);
+            }
+
+            return image;
+        }
+
+        public Image GetImageWithSourceFile(string id)
+        {
+            Image image = GetImage(id);
+            image.Content = imageRepository.FindSourceFile(id);
+            return image;
+        }
+
+        public Image GetImageWithThumbnailFile(string id)
+        {
+            Image image = GetImage(id);
+            image.Content = imageRepository.FindThumbnailFile(id);
+            return image;
+        }
+
+        public Upload GetUpload(string id)
+        {
+            Upload? upload = uploadRepository.Read(id);
+            if (upload is null)
+            {
+                throw new NotFoundException(typeof(Upload), id);
+            }
+            return upload;
+        }
+
+        public Upload GetUploadWithSourceFiles(string id)
+        {
+            Upload upload = GetUpload(id);
+            FillUploadWithFiles(upload, imageRepository.FindSourceFile);
+            return upload;
+        }
+
+        public Upload GetUploadWithThumbnailFiles(string id)
+        {
+            Upload upload = GetUpload(id);
+            FillUploadWithFiles(upload, imageRepository.FindThumbnailFile);
+            return upload;
+        }
+
+        public ValidationConfiguration GetValidationConfiguration()
+        {
+            return Configuration.Validation;
+        }
+
+        public void PostUpload(Upload upload)
+        {
+            ValidateUpload(upload);
+            SetUpload(upload);
+            SetImages(upload);
+            uploadRepository.Create(upload);
+        }
+
+        protected virtual void ValidateUpload(Upload upload)
+        {
+            if (upload.Images.Count == 0 || upload.Images.Count > Configuration.Validation.MaxImagePerUpload)
+            {
+                throw new InvalidEntityException(typeof(Upload), nameof(upload.Images.Count), upload.Images.Count);
+            }
+
+            foreach (var item in upload.Images)
+            {
+                if (item.Content is null)
+                {
+                    throw new InvalidEntityException(typeof(Image), nameof(item.Content), item.Content);
+                }
+
+                if (item.Content.Length > Configuration.Validation.MaxFileSize)
+                {
+                    throw new InvalidEntityException(typeof(Image), nameof(item.Content.Length), item.Content.Length);
+                }
+
+                if (!Configuration.Validation.SupportedFormats.Contains(Path.GetExtension(item.Content.FileName).TrimStart('.').ToLower()))
+                {
+                    throw new InvalidEntityException(typeof(Image), nameof(item.Content.ContentType), item.Content.ContentType);
+                }
+
+                if (item.Description != null && Configuration.Validation.DescriptionMaxLength < item.Description.Length)
+                {
+                    throw new InvalidEntityException(typeof(Image), nameof(item.Description), item.Description.Length);
+                }
+            }
+        }
+
+        private void FillUploadWithFiles(Upload? upload, Func<string, IFormFile?> find)
+        {
+            if (upload is not null)
+            {
+                foreach (var item in upload.Images)
+                {
+                    item.Content = find(item.Id);
+                }
+            }
+        }
+
+        private string GenerateId(IEnumerable<string> repositoryKeys)
         {
             string id;
             do
             {
                 id = Guid.NewGuid().ToString().Replace("-", "").ToLower();
-            } while (collection.FirstOrDefault(key => key == id) != null);
+            } while (repositoryKeys.FirstOrDefault(key => key == id) != null);
             return id;
         }
 
-        private string[] GenerateMultipleId(IEnumerable<string> collection, int count)
+        private string[] GenerateMultipleId(IEnumerable<string> repositoryKeys, int count)
         {
             string[] ids = new string[count];
             for (int i = 0; i < count; i++)
             {
-                ids[i] = GenerateId(collection);
+                ids[i] = GenerateId(repositoryKeys);
             }
             return ids;
         }
-   
 
-    }
+        private PasteImgConfiguration ReadConfiguration()
+        {
+            string configPath = Path.Combine("Properties", "pasteImgLogic.json");
+            try
+            {
+                return PasteImgConfiguration.ReadConfiguration(configPath);
+            }
+            catch
+            {
+                PasteImgConfiguration.WriteConfiguration(configPath, PasteImgConfiguration.Default);
+                return PasteImgConfiguration.ReadConfiguration(configPath);
+            }
+        }
 
-    public class PasteImgLogicConfiguration
-    {
-        public int CompressionQuality { get; }
-        public string ImageRoot { get; }
-        public long? MaxFileSize { get; }
-        public int? MaxImagePerUpload { get; }
-        public (int width, int height) MaxSize { get; }
-        public (int width, int height) MaxThumbnailSize { get; }
-        public ReadOnlyCollection<string> SupportedFormats { get; }
-        public string ThumbnailRoot { get; }
+        private void SetImages(Upload upload)
+        {
+            int count = upload.Images.Count;
+            string[] ids = GenerateMultipleId(GetAllImage().Select(i => i.Id), count);
+            for (int i = 0; i < count; i++)
+            {
+                Image image = upload.Images[i];
+                image.Id = ids[i];
+                image.UploadID = upload.Id;
+                imageRepository.Create(image, thumbnailCreator: thumbnailCreator);
+                backgroundTransformer.EnqueueRequest
+                    (new BackgroundTransformationRequest(imageRepository.GetSourcePath(image.Id), sourceOptimizer));
+            }
+        }
+
+        private void SetUpload(Upload upload)
+        {
+            upload.Id = GenerateId(uploadRepository.ReadAll().Select(i => i.Id));
+            upload.Password = CreateHash(upload.Password);
+            upload.TimeStamp = DateTime.Now;
+        }
     }
 }
