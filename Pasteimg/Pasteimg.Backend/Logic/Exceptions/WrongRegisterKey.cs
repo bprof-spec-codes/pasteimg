3c1744eebf5c29d2f17dd89c81d102b80114a244
Máté Nagy <nm20010214@gmail.com>
2023-05-09 10:18:32 +0200

    Created exception for wrong regKey

---
+++ Pasteimg/Pasteimg.Backend/Logic/Exceptions/WrongRegisterKey.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Pasteimg.Backend.Logic.Exceptions
+{
+    [HttpError(HttpStatusCode.Conflict, 777)]
+    public class WrongRegisterKey : Exception
+    {
+        public WrongRegisterKey(): base("Wrong register key") { }
+    }
+}
