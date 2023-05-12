6328cc18098d8555009e86704ca0c2616256e8ba
kklevi96 <kklevi@gmail.com>
2023-05-12 21:10:42 +0200

    upload form component base added

---
+++ Pasteimg.Frontend/src/app/new-upload/new-upload.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-upload',
+  templateUrl: './new-upload.component.html',
+  styleUrls: ['./new-upload.component.scss']
+})
+export class NewUploadComponent {
+
+}
