4d0005bd51c7706e96b3ca6a469f89693973f86f
kklevi96 <kklevi@gmail.com>
2023-05-12 21:11:22 +0200

    view upload link

--- Pasteimg.Frontend/src/app/app-routing.module.ts
+++ Pasteimg.Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ViewUploadComponent} from "./view-upload/view-upload.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "link/:uploadId", component: ViewUploadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
