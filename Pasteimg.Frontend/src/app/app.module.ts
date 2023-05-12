346dbd8c21429994c6d9e3816c7ce3577f50583a
kklevi96 <kklevi@gmail.com>
2023-05-12 21:13:38 +0200

    Revert "remove unnecessary example"
    This reverts commit b46fdd69bb688a34d796374f146c37252914ce65.

--- Pasteimg.Frontend/src/app/app.module.ts
+++ Pasteimg.Frontend/src/app/app.module.ts
@@ -4,36 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewUploadComponent } from './view-upload/view-upload.component';
-import { HomeComponent } from './home/home.component';
-
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatMenuModule} from "@angular/material/menu";
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
-import { NewUploadComponent } from './new-upload/new-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewUploadComponent,
-    HomeComponent,
-    NavigationComponent,
-    FooterComponent,
-    NewUploadComponent,
+    ViewUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatListModule,
-    MatIconModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
