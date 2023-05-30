2769ac3b8c848cec8ef1f4b39141c84636577884
Nagy Máté <nm20010214@gmail.com>
2023-05-31 01:41:40 +0200

    Login added

--- Pasteimg.Frontend/src/app/login/login.component.ts
+++ Pasteimg.Frontend/src/app/login/login.component.ts
@@ -1,35 +1,73 @@
 import { Component } from '@angular/core';
-import { UploadService } from "../upload.service";
+import { LoginModel } from '../_models/loginModel';
+import { FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { sessionIdService } from '../sessionId.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-  constructor(
-    private router: Router,
-    private uploadService: UploadService
-  ) {}
 
-  async ngOnInit(): Promise<void> {
-    if (await this.uploadService.checkSessionIsAdmin()) 
-    {
-      this.router.navigate(['/upload']);
-    }
+export class LoginComponent {
+  loginModel: LoginModel
+  sessionIdService: sessionIdService
+  email: FormControl
+  password: FormControl
+  http:HttpClient
+  header: HttpHeaders
+  snackBar: MatSnackBar
+  nav: Router
+
+
+  constructor(http: HttpClient, snackBar: MatSnackBar, nav: Router, idService: sessionIdService) {
+    this.loginModel = new LoginModel()
+    this.http = http
+    this.snackBar = snackBar
+    this.nav = nav
+    this.sessionIdService = idService
+    
+    this.password = new FormControl('', [Validators.required])
+    this.email = new FormControl('', [Validators.required, Validators.email])
+
+    this.sessionIdService.getSessionId()
+
+    this.header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      //'SessionKeyHeader' : localStorage.getItem('sessionId')!.toString(),
+      'API-SESSION-KEY' : localStorage.getItem('sessionId')!.toString()
+    })  
   }
 
-  email: string = "";
-  password: string= "";
-
-  async login(): Promise<void> {
-    const isLoginSuccessfull = await this.uploadService.submitLogin(this.email, this.password);
-    if (isLoginSuccessfull) {
-      this.router.navigate(['/upload']);
-    } else {
-      // Handle login failure
-      console.log('Login failed');
+  public getPasswordErrorMessage() : string {
+    if (this.password.hasError('required')) {
+      return 'You must enter a password!';
     }
+    return '';
+  }
+
+  public getEmailErrorMessage() : string {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value!';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email!' : '';
+  }
+
+  public send(){
+    console.log('sending');
+    
+    this.http.post('https://localhost:7063/api/Admin/Login', this.loginModel, {headers: this.header}).subscribe(
+      (success) => {
+        this.nav.navigate(['/admin'])
+      },
+      (error) => {
+        console.log(error)
+        this.snackBar.open(error.message, 'Close', { duration: 5000 })
+      })
   }
 }
