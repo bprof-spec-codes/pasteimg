ab270445c64b7bb8212ca43b9e4c7bba1df42829
samu112 <lerantsamuel@gmail.com>
2023-06-01 04:07:55 +0200

    Images displayyed by sending header in message

--- Pasteimg.Frontend/src/app/view-upload/view-upload.component.ts
+++ Pasteimg.Frontend/src/app/view-upload/view-upload.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {Upload, UploadService} from "../upload.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Upload, UploadService } from "../upload.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { sessionIdService } from "../sessionId.service";
 
 @Component({
   selector: 'app-view-upload',
@@ -11,30 +13,46 @@ export class ViewUploadComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private uploadService: UploadService
+    private uploadService: UploadService,
+    private http: HttpClient,
+    private sessionIdService: sessionIdService
   ) {}
 
-  upload!: Upload
+  upload!: Upload;
+  images: any[] = []; // Array to store the images
 
-
-  ngOnInit() {
+  async ngOnInit() {
+    const sessionKey = await this.sessionIdService.getSessionId();
     const routeParams = this.route.snapshot.paramMap;
     const uploadIdFromRoute = String(routeParams.get('uploadId'));
 
-    this.uploadService.getUpload(uploadIdFromRoute).then(u => {
-      this.upload = u
+    await this.uploadService.getUpload(uploadIdFromRoute).then(u => {
+      this.upload = u;
       //console.log(u)
-     })
-     .catch(error => {
+    })
+    .catch(error => {
       if (error.status === 404) {
         console.error('Image doesn\'t exist', error);
       }
       else if (error.status === 401) {
-        this.router.navigate(['/password/'+this.upload.id]);
+        this.router.navigate(['/password/' + uploadIdFromRoute]);
       }
-      else{
+      else {
         console.error('An error occurred:', error);
       }
     });
-  };
+
+    const headers = new HttpHeaders().set('API-SESSION-KEY', sessionKey || '');
+    // Iterate through each image ID and make separate HTTP requests
+    for (const image of this.upload.images) {
+      const imageId = image.id;
+      const imageDescription = image.description;
+      const imageNsfw = image.nsfw;
+      this.http.get<any>('https://localhost:7063/api/Public/GetImageWithSourceFile/' + image.id, { headers }).subscribe(p => {
+        const contentType = p.content.contentType;
+        const data = p.content.data;
+        this.images.push({ imageId, imageDescription, imageNsfw, contentType , data,  }); // Push the image data to the array
+      });
+    }
   }
+}
